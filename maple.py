522a3d423d480465ea89eecf9b7b1247c8c4dd21
Dimitar Dimitrov <newbrict@gmail.com>
2016-08-16 22:51:54 -0400

    reads in all the files, time for the real work

--- maple.py
+++ maple.py
@@ -1,50 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-__all__ = ['Scroll', 'Equip', 'Attributes'] 
-
-ALL_ATTRIBUTES = [
-'str',
-'dex',
-'int',
-'luk',
-'hp',
-'mp',
-'speed',
-'jump',
-'watt',
-'matt',
-'wdef',
-'mdef',
-'avoidability',
-'accuracy']
-
-class Attribute(object):
-
-	def __init__(self, name=None, value=0):
-		self._name = name
-		self._value = value
-
-	def _get_name(self):
-		return self._name
-
-	def _set_name(self, new_name):
-		self._name = new_name
-
-	name = property(_get_name, _set_name)
-
-	def _get_value(self):
-		return self._value
-
-	def _set_value(self, new_value):
-		self._value = new_value
-
-	value = property(_get_value, _set_value)
-
-	def __repr__(self):
-		return "'{}': {}".format(self._name, self._value)
-
-	__str__ = __repr__
+__all__ = ['Scroll', 'Equip'] 
 
 class AbstractItem(object):
 
@@ -79,7 +36,7 @@ class AbstractItem(object):
 	attributes = property(_get_attributes, _set_attributes)
 
 	def __repr__(self):
-		return "name: {}, price:{}, attributes:{}".format(self._name, self._price, "attr")
+		return 'name: {}, price:{}, attributes:{}'.format(self._name, self._price, self._attributes)
 
 	__str__ = __repr__
 
@@ -88,8 +45,16 @@ class Equip(AbstractItem):
 		AbstractItem.__init__(self)
 		self._slots = None
 
+	def _get_slots(self):
+		return self._slots
+
+	def _set_slots(self, new_slots):
+		self._slots = new_slots
+
+	slots = property(_get_slots, _set_slots)
+	
 	def __repr__(self):
-		return "{}, slots:{}".format(AbstractItem.__str__(self), self._slots)
+		return '{}, slots:{}'.format(AbstractItem.__str__(self), self._slots)
 
 	__str__ = __repr__
 
@@ -116,7 +81,7 @@ class Scroll(AbstractItem):
 	chance_destroy = property(_get_chance_destroy, _set_chance_destroy)
 
 	def __repr__(self):
-		return "{}, chance_success:{}, chance_destroy:{}".format(AbstractItem.__str__(self), self._chance_success, self._chance_destroy)
+		return '{}, chance_success:{}, chance_destroy:{}'.format(AbstractItem.__str__(self), self._chance_success, self._chance_destroy)
 
 	__str__ = __repr__
 
--- scroller.py
+++ scroller.py
@@ -1,35 +1,81 @@
 #!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
 
-from pprint import pprint
 import json
 import os
+import sys
+import argparse
 
-from maple import Scroll, Equip, Attribute
+from pprint import pprint
 
-SCROLLS_DIRECTORY = 'scrolls'
+from maple import Scroll, Equip
 
-def main():
-	print 'Welcome to Dimitars Ding Dong Scroll Script'
+def process_arguments(argv):
+	parser = argparse.ArgumentParser(description='Maplestory scroll cruncher')
 
+	parser.add_argument('scroll_directory',
+		metavar='SCROLL_DIRECTORY',
+		help='The directory containing all scroll json files for this crunch')
+
+	parser.add_argument('-i', '--input-equip',
+		metavar='INPUT_EQUIP',
+		dest='input_equip',
+		required=True,
+		help='The json file representing the equip you are starting with')
+
+	parser.add_argument('-o', '--output-equip',
+		metavar='OUTPUT_EQUIP',
+		dest='output_equip',
+		required=True,
+		help='The json file representing the equip you want to end up with')
+
+	return parser.parse_args(argv[1:])
+
+# Can throw OSError
+def parse_input_file(filename, input_object):
+	with open(filename) as object_data:
+		object_json = json.load(object_data)
+		for attr, value in object_json.iteritems():
+			setattr(input_object, attr, value)
+
+	return input_object
+
+def main(argv):
+	args = process_arguments(argv)
 
 	scrolls = []
 
 	# Read through all our scroll files and create scroll objects from them
-	available_scroll_files = os.listdir(SCROLLS_DIRECTORY)
-	for scroll_file in available_scroll_files:
-		with open(os.path.join(SCROLLS_DIRECTORY, scroll_file)) as scroll_data:
-			scroll_json = json.load(scroll_data)
-			new_scroll = Scroll()
-			for attr, value in scroll_json.iteritems():
-				setattr(new_scroll, attr, value)
-			scrolls.append(new_scroll)
+	try:
+		available_scroll_files = os.listdir(args.scroll_directory)
+		for scroll_file in available_scroll_files:
+				new_scroll = parse_input_file(os.path.join(args.scroll_directory, scroll_file), Scroll())
+				scrolls.append(new_scroll)
+	except OSError:
+		print "There was an error trying to read files from {}, make sure it exists".format(args.scroll_directory)
+		return 1
+
+	# Read the input equip file and create an equip object from it
+	try:
+		input_equip = parse_input_file(args.input_equip, Equip())
+	except OSError:
+		print "There was an error trying to {}, make sure it exists".format(args.input_equip)
+		return 2
+
+	# Read the output equip file and create an equip object from it
+	try:
+		output_equip = parse_input_file(args.output_equip, Equip())
+	except OSError:
+		print "There was an error trying to {}, make sure it exists".format(args.output_equip)
+		return 2
 
 
 	pprint(scrolls)
+	pprint(input_equip)
+	pprint(output_equip)
+	return 0
 
 if __name__ == '__main__':
-	main()
-	raw_input('Press enter to exit...')
+	sys.exit(main(sys.argv))
 
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
